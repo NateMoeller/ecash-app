d1c6ab9d214a3659320e12e2fcaa55942622d5ee
Nathan Moeller <n.moeller18@gmail.com>
2026-02-12 23:50:53 -0800

    Add transition

--- lib/pin_gated_app.dart
+++ lib/pin_gated_app.dart
@@ -3,6 +3,8 @@ import 'package:ecashapp/screens/pin_lock_screen.dart';
 import 'package:ecashapp/theme.dart';
 import 'package:flutter/material.dart';
 
+enum _LockState { locked, unlocking, unlocked }
+
 class PinGatedApp extends StatefulWidget {
   final bool pinRequired;
   final Widget child;
@@ -17,19 +19,41 @@ class PinGatedApp extends StatefulWidget {
   State<PinGatedApp> createState() => _PinGatedAppState();
 }
 
-class _PinGatedAppState extends State<PinGatedApp> with WidgetsBindingObserver {
-  bool _locked = false;
+class _PinGatedAppState extends State<PinGatedApp>
+    with WidgetsBindingObserver, SingleTickerProviderStateMixin {
+  _LockState _state = _LockState.unlocked;
   DateTime? _backgroundedAt;
+  late final AnimationController _controller;
+  late final Animation<double> _fadeAnimation;
+  late final Animation<double> _scaleAnimation;
 
   @override
   void initState() {
     super.initState();
-    _locked = widget.pinRequired;
+    _state =
+        widget.pinRequired ? _LockState.locked : _LockState.unlocked;
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _fadeAnimation = Tween<double>(begin: 1.0, end: 0.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInCubic),
+    );
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.05).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInCubic),
+    );
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() => _state = _LockState.unlocked);
+        _controller.reset();
+      }
+    });
     WidgetsBinding.instance.addObserver(this);
   }
 
   @override
   void dispose() {
+    _controller.dispose();
     WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
@@ -52,24 +76,43 @@ class _PinGatedAppState extends State<PinGatedApp> with WidgetsBindingObserver {
     if (elapsed.inSeconds > 30) {
       final pinSet = await hasPinCode();
       if (pinSet && mounted) {
-        setState(() => _locked = true);
+        setState(() => _state = _LockState.locked);
       }
     }
   }
 
   void _unlock() {
-    setState(() => _locked = false);
+    setState(() => _state = _LockState.unlocking);
+    _controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_locked) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: cypherpunkNinjaTheme,
-        home: PinLockScreen(onUnlocked: _unlock),
-      );
-    }
-    return widget.child;
+    return Stack(
+      children: [
+        widget.child,
+        if (_state != _LockState.unlocked)
+          AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              return IgnorePointer(
+                ignoring: _state == _LockState.unlocking,
+                child: FadeTransition(
+                  opacity: _fadeAnimation,
+                  child: ScaleTransition(
+                    scale: _scaleAnimation,
+                    child: child,
+                  ),
+                ),
+              );
+            },
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: cypherpunkNinjaTheme,
+              home: PinLockScreen(onUnlocked: _unlock),
+            ),
+          ),
+      ],
+    );
   }
 }
